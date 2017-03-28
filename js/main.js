a54bf9754db4559a4a28770c711704790bc64865
jbeltrami <joao_beltrami@hotmail.com>
2017-03-28 15:48:19 -0400

    Correct errors on code. It is running now.

--- js/main.js
+++ js/main.js
@@ -1,18 +1,59 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png",
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	},
+
+];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[2];
+var gameBoard = "game-board";
 
-cardsInPlay.push(cardOne);
-console.log ("User filpped" + " " + cards[0]);
-
-cardsInPlay.push(cardTwo);
-console.log ("User filpped" + " " + cards[2]);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay [0] === cardsInPlay [1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
+var createBoard = function () {
+	for (let i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
 	}
 }
+
+var flipCard = function () {
+	var cardId = this.getAttribute('data-id')
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	//console.log('What is this?', this)
+	this.setAttribute('src', cards[cardId].cardImage);
+	//this.getElementsByTagName('img')[0].setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch(flipCard);
+	}
+};
+
+var checkForMatch = function () {
+			if (cardsInPlay [0] === cardsInPlay [1]) {
+			alert("You found a match!");
+		} else {
+			alert("Sorry, try again");
+		}
+};
+
+createBoard ();
